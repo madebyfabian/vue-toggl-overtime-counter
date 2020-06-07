c9f666f24191df9244a4096d49f15ca3642f3d15
madebyfabian <beer.fabian@mail.de>
2020-06-07 16:17:02 +0200

    Gotrue API fixes

--- src/functions/gotrue-auth.js
+++ src/functions/gotrue-auth.js
@@ -1,10 +1,7 @@
 import GoTrue from "gotrue-js"
 
-// Find out if we're on localhost
-const isLocal = document.location.host.split(':').shift() === 'localhost'
-
 export default new GoTrue({
-  APIUrl: "https://frosty-liskov-a5f4a1.netlify.app/.netlify/identity",
+  APIUrl: "https://overtimetrackr.netlify.app/.netlify/identity",
   audience: "",
   setCookie: true
 })
