486eded7199178390505baff8d6533f45ccdd7e1
madebyfabian <beer.fabian@mail.de>
2020-06-14 11:26:25 +0200

    Installed CustomParseFormat

--- src/plugins/Dayjs.js
+++ src/plugins/Dayjs.js
@@ -5,6 +5,11 @@ import dayjs from 'dayjs'
 import 'dayjs/locale/de'
 dayjs.locale('de')
 
+// Extends dayjs() constructor to support custom formats of input strings.
+// e.g. dayjs('2020-05-05', 'YYYY-MM-DD')
+var customParseFormat = require('dayjs/plugin/customParseFormat')
+dayjs.extend(customParseFormat)
+
 // Add "duration" feature
 // import duration from 'dayjs/plugin/duration'
 // dayjs.extend(duration)
