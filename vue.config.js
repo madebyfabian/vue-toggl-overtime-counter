b686bb56160d63a4588141dc9b4465c54781774b
madebyfabian <beer.fabian@mail.de>
2020-06-20 11:01:38 +0200

    Disabled custom webpack function

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,20 @@
 module.exports = {
   productionSourceMap: false,
-  
-  chainWebpack: config => {
-    // Disabling webpack translating small images into base64 strings
-    const imagesRule = config.module.rule('images');
-    imagesRule.uses.clear()
-    imagesRule
-      .test(/\.(png|jpe?g|gif|webp)(\?.*)?$/)
-      .use('file-loader')
-      .loader('file-loader')
-      .tap(options => {
-        return {
-          ...options,
-          limit: -1 // no limit
-        };
-      })
-      .end();
-  }
+
+  // Disabling webpack translating small images into base64 strings
+  // chainWebpack: config => {,
+  //   const imagesRule = config.module.rule('images');
+  //   imagesRule.uses.clear()
+  //   imagesRule
+  //     .test(/\.(png|jpe?g|gif|webp)(\?.*)?$/)
+  //     .use('file-loader')
+  //     .loader('file-loader')
+  //     .tap(options => {
+  //       return {
+  //         ...options,
+  //         limit: -1 // no limit
+  //       };
+  //     })
+  //     .end();
+  // }
 };
